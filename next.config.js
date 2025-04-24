2506e92fb1d10de681375fb28df1087d539ac04f
Mohammadreza <mr.reza0425@gmail.com>
2025-04-25 01:56:23 +0330

    fix errors after vercel

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL || 'https://apnelec13.vercel.app'
+  }
+}
 
 module.exports = nextConfig
