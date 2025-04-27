90d53ef65db7ebd0ee157e5665388695c43132d4
Mohammadreza <mr.reza0425@gmail.com>
2025-04-27 20:22:23 +0330

    Revert "fix errors after vercel"
    This reverts commit 2506e92fb1d10de681375fb28df1087d539ac04f.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL || 'https://apnelec13.vercel.app'
-  }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
